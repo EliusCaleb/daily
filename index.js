68eceb2ad5a061477f4c0a606de9393b06d876f2
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-12-13 13:26:23 +0300

    refactoring

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { getDatabase,ref,push } from 'https://www.gstatic.com/firebasejs/9.15.0/
 
 
 
-const inputVal = document.getElementById("add-input");
+const inputField = document.getElementById("add-input");
 const addBtn = document.getElementById("add-btn");
+const  taskListing = document.getElementById("showing")
 
 const appSetting = {
     databaseURL: "https://notebook-e41b3-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -18,8 +19,21 @@ console.log(app)
 
 
 addBtn.addEventListener("click",function(){
-    const inputValue = inputVal.value
+    const inputValue = inputField.value
      console.log(inputValue)
      console.log(`${inputValue} added to database`)
      push(taskInDb,inputValue)
+
+     addTask(inputValue)
+     clearInput()
+     
 })
+
+
+function clearInput(){
+    inputVal.value= ''
+}
+
+function addTask(itemValue){
+    taskListing.innerHTML +=`<li> ${itemValue}</li>`
+}
